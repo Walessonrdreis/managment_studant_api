c8e316bcce42678cdc8b9c4016ea070b94ea53e0
Walesson <wallllase@hotmail.com>
2025-03-05 07:59:34 -0300

    feat(db_conection): Melhorando script de conexão.

--- scripts/database/db_connection.php
+++ scripts/database/db_connection.php
@@ -3,6 +3,11 @@
 require __DIR__ . '/../../api/vendor/autoload.php'; // Carrega o autoload do Composer
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Dotenv\Dotenv; // Adicionando a importação do Dotenv
+
+// Carregar o arquivo .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../api'); // Ajuste o caminho conforme necessário
+$dotenv->load();
 
 // Configuração do banco de dados
 $capsule = new Capsule;
@@ -22,10 +27,28 @@ $capsule->addConnection([
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
+// Códigos de cor
+$green = "\033[32m"; // Verde
+$red = "\033[31m";   // Vermelho
+$reset = "\033[0m";  // Resetar cor
+
 // Testar a conexão
 try {
-    $capsule::connection()->getPdo();
-    echo "Conexão com o banco de dados estabelecida com sucesso!\n";
+    $connection = $capsule::connection();
+    $connection->getPdo();
+    
+    // Obtendo detalhes da conexão
+    $dbName = $connection->getDatabaseName();
+    $host = $connection->getConfig('host');
+    $username = $connection->getConfig('username');
+
+    echo "{$green}✅ Conexão com o banco de dados estabelecida com sucesso!{$reset}\n";
+    echo "{$green}🔍 Detalhes da Conexão:{$reset}\n";
+    echo "{$green}📦 Banco de Dados: {$dbName}{$reset}\n";
+    echo "{$green}🌐 Host: {$host}{$reset}\n";
+    echo "{$green}👤 Usuário: {$username}{$reset}\n";
 } catch (\Exception $e) {
-    echo "Erro ao conectar ao banco de dados: " . $e->getMessage() . "\n";
+    echo "{$red}❌ Erro ao conectar ao banco de dados: {$e->getMessage()}{$reset}\n";
+    echo "{$red}📝 Código do Erro: {$e->getCode()}{$reset}\n";
+    echo "{$red}📜 Log do Erro: {$e->getTraceAsString()}{$reset}\n"; // Exibe a pilha de chamadas
 }
