35d891877badd62d33ea191ead491fad3d2ec31e
Walesson <wallllase@hotmail.com>
2025-03-05 21:43:35 -0300

    test: garantir que os testes do SchoolController estejam OK - Executar todos os testes do SchoolController e confirmar que todos passaram com sucesso. - Validar a funcionalidade de criação, leitura, atualização e exclusão de escolas. - Assegurar que as alterações recentes não introduziram novos erros nas funcionalidades do SchoolController.

--- api/app/Models/School.php
+++ api/app/Models/School.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class School extends Model
 {
     use HasFactory;
@@ -12,6 +13,8 @@ class School extends Model
      * Modelo para gerenciar escolas.
      * Este modelo representa a tabela de escolas no banco de dados.
      */
+
+    protected $fillable = ['name'];
 
     /**
      * Define a relação com o modelo User.
