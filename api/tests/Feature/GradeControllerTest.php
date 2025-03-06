9e88bd4e8322b163d52f21219e39ffa44f3129c8
Walesson <wallllase@hotmail.com>
2025-03-06 12:44:18 -0300

    test: adicionar testes para o GradeController - Implementar testes para criar, listar, mostrar, atualizar e excluir notas. - Garantir que as funcionalidades do GradeController estejam funcionando corretamente com base nas operações de notas dos estudantes. - Verificar se as respostas da API estão corretas e se as mensagens de sucesso são retornadas conforme esperado.

--- api/tests/Feature/GradeControllerTest.php
+++ api/tests/Feature/GradeControllerTest.php
@@ -5,6 +5,10 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Grade;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Student;
+use App\Models\Subject;
 
 class GradeControllerTest extends TestCase
 {
@@ -13,12 +17,29 @@ class GradeControllerTest extends TestCase
     /** @test */
     public function it_can_create_a_grade()
     {
-        $response = $this->postJson('/api/grades', [
-            'student_id' => 1,
-            'subject_id' => 1,
-            'grade' => 90,
-        ]);
+        // Crie um papel específico para o estudante
+        $role = Role::firstOrCreate(['name' => 'student']);
 
+        // Crie um usuário e atribua o papel de estudante
+        $user = User::factory()->create(['role_id' => $role->id]);
+
+        // Crie um estudante associado ao usuário
+        $student = Student::factory()->create(['user_id' => $user->id]);
+
+        // Crie uma disciplina
+        $subject = Subject::factory()->create(); // Cria uma disciplina e obtém o ID
+
+        // Dados para a requisição
+        $data = [
+            'student_id' => $student->id, // Use o ID do estudante criado
+            'subject_id' => $subject->id, // Use o ID da disciplina criada
+            'grade' => 90,
+        ];
+
+        // Faça a requisição para criar a nota
+        $response = $this->postJson('/api/grades', $data);
+
+        // Verifique se a resposta está correta
         $response->assertStatus(201)
                  ->assertJson(['success' => true, 'message' => 'Nota criada com sucesso']);
     }
